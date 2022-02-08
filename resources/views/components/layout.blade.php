fdb406527d4857f60371ed8994b269847a849872
Leo <aammlj2002@gmail.com>
2022-02-08 11:49:09 +0630

    styling index page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    @livewireStyles
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
+
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <div class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <h3 class="font-bold">Video Game Aggregator</h3>
+                </a>
+                <ul class="flex ml-16 space-x-4">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text"
+                        class="bg-gray-800 text-sm w-64 focus:outline-none focus:shadow-outline rounded-full pl-8 px-3 py-1"
+                        placeholder="Search...">
+                    <div class="absolute top-0 flex item-center h-full ml-2">
+                        <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24">
+                            <path class="heroicon-ui"
+                                d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="/"><img src="{{asset('img/marco.jpg')}}" alt="avatar" class="rounded-full w-8 h-8"></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main class="py-4">
+        <div class="container mx-auto px-4">
+            {{$slot}}
+        </div>
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            <p>Powered By <a href="#" class="underline hover:text-gray-400">IGDB API</a></p>
+        </div>
+    </footer>
+
+    @livewireScripts
+    <script src="{{mix('js/app.js')}}"></script>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,113 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-layout>
+    <h2 class="text-blue-500 tracking-wide uppercase font-semibold ">Popular Games</h2>
+    {{-- end popular game section --}}
+    <div class="popular-game text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        @foreach (range(1,9) as $item)
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="{{asset('img/ff7.jpg')}}" alt="game"
+                        class="w-48 hover:opacity-75 trasition ease-in-out duration-150">
+                </a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px; bottom:-20px">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        88%
+                    </div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-light hover:text-gray-400 mt-8">Final Fantasy 7
+                Remake</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+        @endforeach
+    </div>{{-- end popular game section --}}
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- review section section --}}
+    <div class="flex my-10">
+        <div class="recent-review w-3/4 mr-32">
+            <div class="text-blue-500 uppercase tracking-wide font-semibold">
+                Recent Reviewed
+            </div>
+            <div class="recently-reviewed-container space-y-12 mt-8">
+                @foreach (range(1,4) as $item)
+                {{-- game review card --}}
+                <div class="game bg-gray-800 rounded-lg shadow-md flex p-6">
+                    <div class="relative flex-none">
+                        <a href="#">
+                            <img src="{{asset('img/ff7.jpg')}}" alt="game"
+                                class="w-48 hover:opacity-75 trasition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                            style="right:-20px; bottom:-20px">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                88%
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ml-12">
+                        <a href="" class="block text-base font-semibold leading-light hover:text-gray-400">Final
+                            Fantasy 7 Remake</a>
+                        <div class="text-gray-400 mt-1">Playstation 4</div>
+                        <div class="mt-6 text-gray-400">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae itaque assumenda
+                            officiis facilis, nulla eligendi, deleniti dolorum fugit, eaque tempora vitae. Officiis
+                            laboriosam doloremque nesciunt molestiae eaque veniam aut omnis.
+                        </div>
+                    </div>
+                </div>
+                {{-- end game review card --}}
+                @endforeach
+            </div>
+        </div>
+        <div class="most-anticipated w-1/4">
 
-    <title>Laravel</title>
-    @livewireStyles
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
-</head>
+            {{-- most-anticipated section --}}
+            <div class="text-blue-500 uppercase tracking-wide font-semibold">
+                Most Anticipated
+            </div>
+            <div class="most-anticipated-container space-y-10 mt-5">
 
-<body x-data="{message: 'hello'}">
-    <h1 x-text="message" class="text-3xl font-bold underline"></h1>
-    @livewireScripts
-    <script src="{{mix('js/app.js')}}"></script>
-</body>
+                {{-- most-anticipated game card --}}
+                @foreach (range(1,4) as $item)
+                <div class="game flex">
+                    <a href="#">
+                        <img src="{{asset('img/ff7.jpg')}}" alt="game"
+                            class="w-16 hover:opacity-75 trasition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                    </div>
+                </div>
+                @endforeach
+                {{-- end most-anticipated game card --}}
+            </div>
+            {{-- end most-anticipated section --}}
 
-</html>
+            {{-- coming section --}}
+            <div class="text-blue-500 uppercase tracking-wide font-semibold mt-10">
+                Coming Soon
+            </div>
+            <div class="most-anticipated-container space-y-10 mt-5">
+
+                {{-- coming soon game card --}}
+                @foreach (range(1,4) as $item)
+                <div class="game flex">
+                    <a href="#">
+                        <img src="{{asset('img/ff7.jpg')}}" alt="game"
+                            class="w-16 hover:opacity-75 trasition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                    </div>
+                </div>{{-- end coming soon game card --}}
+                @endforeach
+
+            </div>{{-- end coming section --}}
+
+        </div>
+    </div>{{-- end review section section --}}
+
+</x-layout>
