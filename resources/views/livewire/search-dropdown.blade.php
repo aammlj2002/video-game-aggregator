429f080f209e0aa87a73459fdd34aa60c28bb9d3
Leo <aammlj2002@gmail.com>
2022-02-15 13:31:13 +0630

    make search dropdown better UX with alpinejs

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,7 +1,8 @@
-<div class="relative">
+<div class="relative" x-data="{ isVisible : true }" @click.away="isVisible = false">
     <input wire:model="search" type="text"
         class="bg-gray-800 text-sm w-56 md:w-64 focus:outline-none focus:shadow-outline rounded-full px-8 py-1"
-        placeholder="Search...">
+        placeholder="Search..." @focus="isVisible=true" @keydown.window.escape="isVisible=false"
+        @keydown="isVisible=true" @keydown.shift.tab="isVisible=false">
     <div class="absolute top-0 flex item-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24">
             <path class="heroicon-ui"
@@ -10,12 +11,13 @@
     </div>
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-3" style="position: absolute"></div>
     @if(strlen($search) > 1)
-    <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+    <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2" x-show="isVisible"
+        x-transition.opacity.duration.200>
         @if (count($searchResults) > 0)
         <ul>
             @foreach ($searchResults as $game)
             <li class="border-b border-gray-700">
-                <a href="/games/{{$game['slug']}}"
+                <a href="/games/{{$game['slug']}}" @if ($loop->last) @keydown.tab="isVisible=false" @endif
                     class="block hover:gray-700 flex items-center transition ease-in-out duration-150 p-3">
                     <img src="{{$game['coverImage']}}" alt="game" class="w-10">
                     <span class="ml-4">{{$game['name']}}</span>
