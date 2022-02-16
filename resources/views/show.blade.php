c612792cc6818f327854530f8d63be2fccd03c67
Leo <aammlj2002@gmail.com>
2022-02-16 12:49:43 +0630

    added modal to watch trailer video

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -95,8 +95,8 @@
                 </div>
                 <div class="mt-12">{{$game['summary']}}</div>
                 @if ($game['video'])
-                <div class="mt-12">
-                    <a href="https://youtube.com/watch/{{$game['video']}}"
+                <div class="mt-12" x-data="{ trailerModalVisible : false }">
+                    <button @click="trailerModalVisible=true" @keydown.window.escape="trailerModalVisible=false"
                         class="flex bg-blue-500 text-white font-semibold p-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
                         <svg class="w-6 fill-current" viewBox="0 0 24 24">
                             <path d="M0 0h24v24H0z" fill="none"></path>
@@ -105,7 +105,33 @@
                             </path>
                         </svg>
                         <span class="ml-2">Play Trailer</span>
-                    </a>
+                    </button>
+                    <template x-if="trailerModalVisible">
+                        {{-- this is modal --}}
+                        <div x-show="trailerModalVisible" style="background-color: rgba(0, 0, 0, .5);"
+                            class="z-50 fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                            @click="trailerModalVisible=false">
+                            <div class="fixed container mx-auto my-10 lg:px-60 rounded-lg overflow-y-auto">
+                                <div class="bg-gray-900 rounded">
+                                    <div class="flex justify-end pr-4 pt-2">
+                                        <button class="text-3xl leading-none hover:text-gray-300"
+                                            @click="trailerModalVisible=false">
+                                            &times;
+                                        </button>
+                                    </div>
+                                    <div class="modal-body px-8 py-8">
+                                        <div class="responsive-container overflow-hidden relative"
+                                            style="padding-top: 56.25%">
+                                            <iframe width="560" height="315"
+                                                class="responsive-iframe absolute top-0 left-0 w-full h-full"
+                                                src="https://youtube.com/embed/{{ $game['video'] }}" style="border:0;"
+                                                allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
                 </div>
                 @endif
             </div>
